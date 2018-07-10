524377788775f6cada640f71c18c3394ebe559c7
jsen <jacobsenecal@yahoo.com>
2018-07-09 23:40:09 -0600

    adapted flask framework

---
+++ app/main/routes.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+from flask import render_template, flash, redirect, url_for, request, g, \
+    jsonify, current_app
+from app import db
+from app.main.forms import EditProfileForm, PostForm
+from app.main import bp
+
+
+@bp.route('/')
+@bp.route('/index')
+def index():
+    return render_template('index.html', title='Home')
+
+@bp.route('/resume')
+def resume():
+    return "put resume here"
+
+@bp.route('/about')
+def about():
+    return "put about here"
+
+    
+
+
