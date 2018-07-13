fc56dd9dce32d68bb1e1881da60a9ea4484e3994
senecal-jjs <jacobsenecal@yahoo.com>
2018-07-12 20:33:21 -0600

    pagination working

--- app/main/routes.py
+++ app/main/routes.py
@@ -3,12 +3,18 @@ from flask import render_template, flash, redirect, url_for, request, g, \
     jsonify, current_app
 from app import db
 from app.main import bp
+from app.models import Post 
 
 
 @bp.route('/')
 @bp.route('/index')
 def index():
-    return render_template('index.html', title='Home')
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.timestamp.desc()).paginate(page, current_app.config['POSTS_PER_PAGE'], False) 
+    next_url = url_for('main.index', page=posts.next_num) if posts.has_next else None
+    prev_url = url_for('main.index', page=posts.prev_num) if posts.has_prev else None 
+
+    return render_template('index.html', title='Home', posts=posts.items, next_url=next_url, prev_url=prev_url)
 
 @bp.route('/resume')
 def resume():
--- config.py
+++ config.py
@@ -17,3 +17,4 @@ class Config(object):
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['your-email@example.com']
     DEBUG = True
+    POSTS_PER_PAGE = 3
