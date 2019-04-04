3521085a98e25261aa0b434d0430a29835135f7e
jsen <senecal@mac>
2019-04-03 22:09:53 -0600

    wrote bash script for automatic update

--- app/git_hook/routes.py
+++ app/git_hook/routes.py
@@ -1,24 +1,15 @@
 import hmac
 from flask import request, Blueprint, jsonify, current_app 
 from git import Repo
+from subprocess import Popen, PIPE
 from app.git_hook import bp
 from flask import current_app
 
 @bp.route('/bitbucket', methods=['POST']) 
-def handle_github_hook(): 
+def handle_bitbucket_hook(): 
     """ Entry point for github webhook """
-
-    signature = request.headers.get('X-Hub-Signature') 
-    sha, signature = signature.split('=')
-
-    secret = str.encode(current_app.config.get('GITHUB_SECRET'))
-
-    hashhex = hmac.new(secret, request.data, digestmod='sha1').hexdigest()
-    if hmac.compare_digest(hashhex, signature): 
-        repo = Repo(current_app.config.get('REPO_PATH')) 
-        origin = repo.remotes.origin 
-        origin.pull('--rebase')
-
-        commit = request.json['after'][0:6]
-        print('Repository updated with commit {}'.format(commit))
-    return jsonify({}), 200
+    try:
+        (stdout, stderr) = Popen(['/home/snj/update.sh',], stdout=PIPE).communicate()
+        return 'success'
+    except:
+        return 'failure'
